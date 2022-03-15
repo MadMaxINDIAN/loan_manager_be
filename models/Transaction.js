81446d7b6b46195be72c156d977de5b5684fa047
Samyak Shah <samyakshah123@gmail.com>
2022-03-15 21:10:39 +0530

    Added manual date

--- models/Transaction.js
+++ models/Transaction.js
@@ -1,20 +1,19 @@
 const mongoose = require("mongoose");
 
-const TransactionSchema = new mongoose.Schema(
-  {
-    amount: {
-      type: Number,
-      required: true,
-    },
-    loan_account_id: {
-      type: mongoose.Schema.Types.ObjectId,
-      required: true,
-      ref: "Loan",
-    },
+const TransactionSchema = new mongoose.Schema({
+  amount: {
+    type: Number,
+    required: true,
   },
-  {
-    timestamps: true,
-  }
-);
+  loan_account_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: "Loan",
+  },
+  date: {
+    type: Date,
+    required: true,
+  },
+});
 
 module.exports = mongoose.model("Transaction", TransactionSchema);
