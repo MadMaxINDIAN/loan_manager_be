5e0de3f2710dedd35d42321b5cee3ba6f23afa6a
Naman Khater <naman22khater@gmail.com>
2022-03-16 14:49:20 +0530

    minor log added

--- server.js
+++ server.js
@@ -29,5 +29,5 @@ app.use('/transaction', transactionRouter);
 
 // listening to port
 app.listen(process.env.PORT || 5000, () => {
-    console.log('Server started on port 3000');
+    console.log(`Server started on port ${process.env.PORT || 5000}`);
 });
