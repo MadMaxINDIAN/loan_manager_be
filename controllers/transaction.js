1f7807b66e5aba2d6411de2787913c1eb3526051
Naman Khater <naman22khater@gmail.com>
2022-03-19 18:33:12 +0530

    basic functionality completed

--- controllers/transaction.js
+++ controllers/transaction.js
@@ -2,11 +2,24 @@ const Loan = require("../models/Loan");
 const Transaction = require("../models/Transaction");
 const { validationResult } = require("express-validator");
 const Borrower = require("../models/Borrowers");
+const Summary = require("../models/Summary");
 
 exports.addTransaction = (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
+    }
+    const today = new Date(req.body.date);
+    var curMonth = today.getMonth();
+
+    var fiscalYr = "";
+    if (curMonth > 2) {
+        //
+        var nextYr1 = (today.getFullYear() + 1).toString();
+        fiscalYr = today.getFullYear().toString() + "-" + nextYr1;
+    } else {
+        var nextYr2 = today.getFullYear().toString();
+        fiscalYr = (today.getFullYear() - 1).toString() + "-" + nextYr2;
     }
     const id = req.params.id;
     let { amount, date } = req.body;
@@ -33,7 +46,7 @@ exports.addTransaction = (req, res) => {
                     loan_account_id: id,
                     date,
                 });
-                transaction.save((err, transaction) => {
+                transaction.save(async (err, transaction) => {
                     if (err) {
                         console.log(err);
                         res.status(500).json({
@@ -41,33 +54,43 @@ exports.addTransaction = (req, res) => {
                             err,
                         });
                     } else {
+                        const summary = await Summary.findOne({
+                            fin_year: fiscalYr,
+                        });
+                        if (!summary) {
+                            const newSummary = new Summary({
+                                fin_year: fiscalYr,
+                                amount_taken: req.body.amount,
+                                amount_invested: 0,
+                            });
+                            await newSummary.save();
+                        } else {
+                            summary.amount_taken += +req.body.amount;
+                            await summary.save();
+                        }
                         Borrower.findById(loan.borrower_id)
                             .then((borrower) => {
                                 if (!borrower) {
-                                    return res
-                                        .status(404)
-                                        .json({
-                                            message: "Borrower not found",
-                                        });
+                                    return res.status(404).json({
+                                        message: "Borrower not found",
+                                    });
                                 }
 
-                                return Loan.find({ borrower_id: loan.borrower_id }).then(
-                                    (loans) => {
-                                        if (!loans) {
-                                            return res
-                                                .status(404)
-                                                .json({
-                                                    message: "Loans not found",
-                                                });
-                                        }
-                                        return res.json({
-                                            message: "Entry done successfully",
-                                            borrower,
-                                            loans,
-                                            transaction
+                                return Loan.find({
+                                    borrower_id: loan.borrower_id,
+                                }).then((loans) => {
+                                    if (!loans) {
+                                        return res.status(404).json({
+                                            message: "Loans not found",
                                         });
                                     }
-                                );
+                                    return res.json({
+                                        message: "Entry done successfully",
+                                        borrower,
+                                        loans,
+                                        transaction,
+                                    });
+                                });
                             })
                             .catch((err) => {
                                 return res.status(500).json({
