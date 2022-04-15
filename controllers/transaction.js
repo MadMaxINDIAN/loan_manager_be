2ce43269a523ea220012f2349ca1c0a1fbb5fab4
Naman Khater <naman22khater@gmail.com>
2022-04-15 10:37:29 +0530

    logs

--- controllers/transaction.js
+++ controllers/transaction.js
@@ -56,7 +56,10 @@ exports.addTransaction = (req, res) => {
       opening_date.setHours(5, 30, 0, 0);
       const time_diff = date.getTime() - opening_date.getTime();
       const day = time_diff / (1000 * 3600 * 24);
-      
+      console.log(day);
+      console.log(ub);
+      console.log(lb);
+      console.log(loan.payments[day]);
       if (day <= 0) {
         return res.status(400).json({
           message: "Entry can not be updated on and before loan opening date",
