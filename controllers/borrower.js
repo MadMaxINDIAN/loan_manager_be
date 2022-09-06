e5523a63869f0721c0b2d356dad827f148fcdf6b
MadMaxINDIAN <naman22khater@gmail.com>
2022-09-06 15:30:00 +0530

    get borrower loan details by loan

--- controllers/borrower.js
+++ controllers/borrower.js
@@ -3,115 +3,115 @@ const { validationResult } = require("express-validator");
 const Loan = require("../models/Loan");
 
 exports.getTotalLoanAccounts = (req, res) => {
-  Loan.find({})
-    .then((loans) => {
-      res.json({
-        success: true,
-        loans: loans.length,
-      });
-    })
-    .catch((err) => {
-      res.json({
-        success: false,
-        message: err.message,
-      });
-    });
+    Loan.find({})
+        .then((loans) => {
+            res.json({
+                success: true,
+                loans: loans.length,
+            });
+        })
+        .catch((err) => {
+            res.json({
+                success: false,
+                message: err.message,
+            });
+        });
 };
 
 exports.addBorrower = (req, res) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res
-      .status(422)
-      .json({ message: "Validation failed", data: errors.array() });
-  }
-  Borrower.findOne({
-    $or: [{ aadhar: req.body.aadhar }, { name: req.body.name }],
-  }).then((borr) => {
-    if (borr) {
-      return res.status(200).json({
-        message: "Borrower found",
-        borrower: borr,
-      });
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res
+            .status(422)
+            .json({ message: "Validation failed", data: errors.array() });
     }
-    const borrower = new Borrower({
-      name: req.body.name,
-      contact: req.body.contact,
-      aadhar: req.body.aadhar.length ? req.body.aadhar : null,
-      occupation: req.body.occupation,
-    });
-    borrower.save((err, borrower) => {
-      if (err) {
-        return res.status(500).json({
-          message:
-            err.message || "Some error occurred while creating the borrower.",
+    Borrower.findOne({
+        $or: [{ aadhar: req.body.aadhar }, { name: req.body.name }],
+    }).then((borr) => {
+        if (borr) {
+            return res.status(200).json({
+                message: "Borrower found",
+                borrower: borr,
+            });
+        }
+        const borrower = new Borrower({
+            name: req.body.name,
+            contact: req.body.contact,
+            aadhar: req.body.aadhar.length ? req.body.aadhar : null,
+            occupation: req.body.occupation,
         });
-      } else {
-        return res.json({ message: "Borrower added", borrower });
-      }
+        borrower.save((err, borrower) => {
+            if (err) {
+                return res.status(500).json({
+                    message:
+                        err.message || "Some error occurred while creating the borrower.",
+                });
+            } else {
+                return res.json({ message: "Borrower added", borrower });
+            }
+        });
     });
-  });
 };
 
 exports.updateBorrower = async (req, res) => {
-  if (req?.user?.type !== "admin") {
-    return res.status(401).json({ message: "Unauthorized" });
-  }
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res
-      .status(422)
-      .json({ message: "Validation failed", data: errors.array() });
-  }
-  const updatedBorrower = await Borrower.findOneAndUpdate(
-    { $or: [{ aadhar: req.body.old_aadhar }, { name: req.body.old_name }] },
-    {
-      name: req.body.name,
-      contact: req.body.contact,
-      aadhar: req.body.aadhar.length ? req.body.aadhar : null,
-      occupation: req.body.occupation,
-    },
-    { new: true }
-  );
-  return res.json({ message: "Borrower updated", borrower: updatedBorrower });
+    if (req?.user?.type !== "admin") {
+        return res.status(401).json({ message: "Unauthorized" });
+    }
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res
+            .status(422)
+            .json({ message: "Validation failed", data: errors.array() });
+    }
+    const updatedBorrower = await Borrower.findOneAndUpdate(
+        { $or: [{ aadhar: req.body.old_aadhar }, { name: req.body.old_name }] },
+        {
+            name: req.body.name,
+            contact: req.body.contact,
+            aadhar: req.body.aadhar.length ? req.body.aadhar : null,
+            occupation: req.body.occupation,
+        },
+        { new: true }
+    );
+    return res.json({ message: "Borrower updated", borrower: updatedBorrower });
 };
 
-exports.getBorrowers = (req, res) => {
-  const name = req.query.search;
-  Borrower.find({ name: { $regex: name, $options: "i" } })
-    .limit(10)
-    .then((borrowers) => {
-      if (!borrowers) {
-        return res.json({ message: "Borrowers not found", borrowers: [] });
-      }
-      return res.json({ message: "Borrowers found", borrowers });
-    })
-    .catch((err) => {
-      return res.status(500).json({
-        message:
-          err.message || "Some error occurred while retrieving borrowers.",
-      });
-    });
+exports.getBorrowers = async (req, res) => {
+    const name = req.query.search;
+    Loan.find({ status: "active" })
+        .populate({ path: "borrower_id", match: { name: { $regex: name, $options: "i" } } })
+        .then(loans => {
+            loans = loans.filter(loan => {
+                return loan.borrower_id !== null;
+            })
+            loans = loans.slice(0, 10)
+            return res.json({ message: "Borrowers found", loans });
+        }).catch((err) => {
+            return res.status(500).json({
+                message:
+                    err.message || "Some error occurred while retrieving borrowers.",
+            });
+        });
 };
 
 exports.getBorrower = (req, res) => {
-  const id = req.params.id;
-  Borrower.findById(id)
-    .then((borrower) => {
-      if (!borrower) {
-        return res.status(404).json({ message: "Borrower not found" });
-      }
-      return Loan.find({ borrower_id: id }).then((loans) => {
-        if (!loans) {
-          return res.status(404).json({ message: "Loans not found" });
-        }
-        return res.json({ message: "Borrower found", borrower, loans });
-      });
-    })
-    .catch((err) => {
-      return res.status(500).json({
-        message:
-          err.message || "Some error occurred while retrieving borrower.",
-      });
-    });
+    const id = req.params.id;
+    Borrower.findById(id)
+        .then((borrower) => {
+            if (!borrower) {
+                return res.status(404).json({ message: "Borrower not found" });
+            }
+            return Loan.find({ borrower_id: id }).then((loans) => {
+                if (!loans) {
+                    return res.status(404).json({ message: "Loans not found" });
+                }
+                return res.json({ message: "Borrower found", borrower, loans });
+            });
+        })
+        .catch((err) => {
+            return res.status(500).json({
+                message:
+                    err.message || "Some error occurred while retrieving borrower.",
+            });
+        });
 };
