9d8ec6c8befb6428ab2038acce328268419eee0b
Samyak Shah <samyakshah123@gmail.com>
2022-09-17 20:41:25 +0530

    Created API for withdrawals and investments

--- controllers/withdraw.js
+++ controllers/withdraw.js
@@ -1,19 +1,19 @@
 const Withdraw = require("../models/Withdraw");
 
 Date.prototype.addDays = function (days) {
-  var date = new Date(this.valueOf());
-  date.setDate(date.getDate() + days);
-  return date;
+    var date = new Date(this.valueOf());
+    date.setDate(date.getDate() + days);
+    return date;
 };
 
 Date.prototype.subtractDays = function (days) {
-  var date = new Date(this.valueOf());
-  date.setDate(date.getDate() - days);
-  return date;
+    var date = new Date(this.valueOf());
+    date.setDate(date.getDate() - days);
+    return date;
 };
 
 exports.getDetailsWithdrawTransaction = async (req, res) => {
-    return res.json({success: true, withdraw: (await Withdraw.find()).map(doc => ({id: doc._id, createdAt: doc.createdAt, name: doc.name, amount: doc.amount, type: doc.type||"Withdraw"}))});
+    return res.json({ success: true, withdraw: (await Withdraw.find()).map(doc => ({ id: doc._id, createdAt: doc.createdAt, name: doc.name, amount: doc.amount, type: doc.type || "Withdraw" })) });
 }
 
 exports.addNewWithdrawTransaction = async (req, res) => {
@@ -34,7 +34,6 @@ exports.addNewWithdrawTransaction = async (req, res) => {
             message: "Amount is required"
         })
     }
-    console.log(req.body);
 
     const withdraw = await Withdraw.create(req.body);
 
@@ -44,3 +43,37 @@ exports.addNewWithdrawTransaction = async (req, res) => {
         withdraw
     })
 };
+
+exports.fetchWithdrawAndAdd = async (req, res) => {
+    if (req.user.type !== 'admin') {
+        res.status(401).json({ message: 'Not authorized' })
+    }
+    try {
+        const withdrawals = await Withdraw.aggregate([
+            {
+                $match: { type: 'Withdraw' }
+            },
+            {
+                $group: {
+                    _id: null,
+                    total: { $sum: "$amount" },
+                },
+            },
+        ])
+        const investments = await Withdraw.aggregate([
+            {
+                $match: { type: 'Add' }
+            },
+            {
+                $group: {
+                    _id: null,
+                    total: { $sum: "$amount" },
+                },
+            },
+        ])
+        res.status(200).json({ message: 'Success', withdrawal: withdrawals[0].total || 0, investment: investments[0].total || 0 })
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({ message: err?.message || 'Something went wrong' })
+    }
+}
--- routers/withdraw.js
+++ routers/withdraw.js
@@ -1,4 +1,4 @@
-const { addNewWithdrawTransaction, getDetailsWithdrawTransaction } = require("../controllers/withdraw");
+const { addNewWithdrawTransaction, getDetailsWithdrawTransaction, fetchWithdrawAndAdd } = require("../controllers/withdraw");
 const authMiddleware = require("../middleware/auth");
 const Withdraw = require("../models/Withdraw");
 const router = require("express").Router();
@@ -7,21 +7,8 @@ const router = require("express").Router();
 // Method: POST
 router.post("/transaction", authMiddleware, addNewWithdrawTransaction);
 
-router.get("/total", authMiddleware, async (req, res) => {
-    const amount = await Withdraw.aggregate([
-        {
-          $group: {
-            _id: null,
-            total: { $sum: "$amount" },
-          },
-        },
-      ]);
-
-      return res.json({
-        total: amount[0].total
-      })
-})
-
 router.get("/transaction", authMiddleware, getDetailsWithdrawTransaction);
+
+router.get('/total', authMiddleware, fetchWithdrawAndAdd)
 
 module.exports = router;
